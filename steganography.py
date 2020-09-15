d49d5f55f867781deec956bd0fe7199b281ced01
Nyein Chan <ninja.aung02@gmail.com>
2020-09-15 04:35:53 -0700

    Removed font folder | Added encode_image function

--- steganography.py
+++ steganography.py
@@ -9,13 +9,13 @@ Deliverables:
     3. Your own image encoded with hidden secret text!
 """
 # TODO: Run `pip3 install Pillow` before running the code.
-from PIL import Image
+from PIL import Image, ImageDraw, ImageChops
 
 
 def decode_image(path_to_png):
     '''
     Args:
-        path_to_png: relative or absolute path to png
+        path_to_png (str): relative or absolute path to png
     
     Returns: 
         decoded image from encoded png
@@ -38,15 +38,15 @@ def decode_image(path_to_png):
 
     decoded_img.save("decoded_image.png")
 
-def encode_image(path_to_png):
+def encode_image(path_to_png, text_to_write):
     """
     TODO: Add docstring and complete implementation.
     """
-    pass
+    raw_image = Image.open(path_to_png)
+    msg_image = Image.new("RGB",raw_image.size,(0,0,0))
 
+    msg = ImageDraw.Draw(msg_image)
+    msg.text((10,10), text_to_write, fill=(1,0,0))
 
-def write_text(text_to_write):
-    """
-    TODO: Add docstring and complete implementation.
-    """
-    pass
+    encoded_image = ImageChops.add(raw_image,msg_image)
+    encoded_image.save('encoded_image.png')
