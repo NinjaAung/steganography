3334bbb1fa8e7e2718864d3aebfa882728b46a4e
Nyein Chan <ninja.aung02@gmail.com>
2020-09-15 18:39:23 -0700

    Finished steganography.py file and organized
    Added round_red_channel for encoding msgs

--- steganography.py
+++ steganography.py
@@ -42,7 +42,7 @@ def encode_image(path_to_png, text_to_write):
     """
     Args:
         path_to_png (str): relative or absolute path to png
-        text_to_write (str): text to encode with
+        text_to_write (str): text to encode with, takes multiline sentences
     
     Returns: 
         encodes image on given file
@@ -52,10 +52,33 @@ def encode_image(path_to_png, text_to_write):
     
     """
     raw_image = Image.open(path_to_png).convert("RGB")
+    round_red_channel(raw_image)
     msg_image = Image.new("RGB",raw_image.size,(0,0,0))
 
     msg = ImageDraw.Draw(msg_image)
-    msg.text((10,10), text_to_write, fill=(1,0,0))
+    msg.multiline_text((10,10), text_to_write, fill=(1,0,0))
+    msg_image.save('Savedtext.png')
 
     encoded_image = ImageChops.add(raw_image,msg_image)
     encoded_image.save('encoded_image.png')
+
+def round_red_channel(image):
+    '''
+    Args:
+        image (PIl image file): realtive or absolute path to png
+    
+    Returns:
+        image with all it's channel rounded down
+    
+    Raises:
+        TypeError: arg image is not a PIL image file
+    '''
+    pixels = image.load()
+    x_size, y_size = image.size
+    for i in range(0,x_size):
+        for j in range(0, y_size):
+            pixel = pixels[i,j]
+            pixels[i,j] = (pixel[0] - pixel[0]%2,pixel[1],pixel[2])
+    
+    
+
